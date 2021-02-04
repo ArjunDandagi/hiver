fd4808c936f830f7eaf1bd8ccfc22a7de715840c
Arjun Dandagi <dandagi.arjun95@gmail.com>
2021-02-05 00:14:06 +0530

    Update list_instances.py

--- list_instances.py
+++ list_instances.py
@@ -3,9 +3,14 @@
 import boto3
 
 ec2=boto3.resource('ec2',region_name="us-east-1")
-instances=ec2.instances.filter()
 
 
+data = ec2.vpcs.filter(Filters=[{'Name':'isDefault','Values':["true"]}]).limit(count=1)
+
+for vpc in data:
+        vpc_id=vpc.vpc_id
+
+instances=ec2.instances.filter(Filters=[{'Name':'vpc-id', 'Values':[vpc_id]},{'Name':'instance-type','Values':['m5.large']}])
 print(f'{"NAME":<20} {"ID":<20} {"PUBLIC_IP":<15} {"STATE":<15}')
 print("="*70)
 
